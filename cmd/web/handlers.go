09d47077290282e9df60b89e23f831ec09977f03
Geovanny Mendoza <Geovanny0401@users.noreply.github.com>
2024-02-03 17:51:09 -0500

    configuration file html and folder static

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -14,10 +14,17 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
+		return
 	}
 
 	err = ts.Execute(w, nil)
@@ -25,7 +32,6 @@ func home(w http.ResponseWriter, r *http.Request) {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 	}
-	w.Write([]byte("Hello from Snippetbox"))
 }
 
 // Add a showSnippet handler function.
