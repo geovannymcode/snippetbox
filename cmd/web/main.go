d58c978603e204b857b52b9da78aebccfcd02aaf
Geovanny Mendoza <Geovanny0401@users.noreply.github.com>
2024-02-04 15:07:54 -0500

    Configuration and Error Handling

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "Http network addrress")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Startting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	infoLog.Printf("Startting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
+	errorLog.Fatal(err)
 }
