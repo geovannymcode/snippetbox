7b55d0aee76f01f7f05efe48dd08a7783f6d032a
Geovanny Mendoza <Geovanny0401@users.noreply.github.com>
2024-02-07 00:12:36 -0500

    Using the SnippetModel

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,12 +7,14 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/Geovanny0401/snippetbox/pkg/models/mysql"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *mysql.SnippetModel
 }
 
 func main() {
@@ -29,9 +31,11 @@ func main() {
 	}
 
 	defer db.Close()
+
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &mysql.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server{
